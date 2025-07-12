659cb570e0fbe88e669e85b1e9dd29429a077419
Marco Antonio Perez Infante <mapi27022005@gmail.com>
2025-07-12 15:52:44 -0400

    *

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [vue()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    assetsInlineLimit: 4096,
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
-  server: {
-    headers: {
-      'Cache-Control': 'no-cache'
-    }
+    assetsDir: 'assets'
   }
 })
